0d202144b71e871ca7bad77ecbcd64cad81a8b52
Midhun Suresh <midhunr@element.io>
2021-12-31 17:35:38 +0530

    Move method down

--- src/types/IMatrixClient.ts
+++ src/types/IMatrixClient.ts
@@ -1,6 +1,21 @@
 export interface IMatrixClient {
+    /**
+     * Register an account with the given credentials; this method must complete all the stages with no further user interaction.
+     */
     register(username: string, password: string, initialDeviceDisplayName: string): Promise<void>;
+
     login(username: string, password: string): Promise<void>;
-    showRoom(roomId: string): Promise<void>;
+
+    /**
+     * Try to start the client with a previous login
+     * @returns true if successful, false otherwise
+     */
     attemptStartWithExistingSession(): Promise<boolean>;
+
+    /**
+     * Renders a timeline for the given room.
+     * @remarks This method should join the room if needed.
+     * @param roomId internal room-id, not alias
+     */
+    showRoom(roomId: string): Promise<void>;
 }
