017ceee9808011c1adb45992c483def924336d45
Midhun Suresh <midhunr@element.io>
2022-01-01 11:41:23 +0530

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      name: 'MyLib',
-	//fileName: (format) => `my-lib.${format}.js`
-      fileName: 'chatterbox.js'
-    }
-  }
-})
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/main.ts"),
+            name: "chatterbox",
+            fileName: "chatterbox.js",
+        },
+    },
+    server: {
+        fs: {
+            // Allow serving files from hydrogen-web/target (for fonts and images)
+            allow: ["../hydrogen-web/target"],
+        },
+    },
+});
