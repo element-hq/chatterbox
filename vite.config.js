33e1b3e0bead14d9f7110674788876e0c11027c2
Midhun Suresh <midhunr@element.io>
2022-01-26 20:04:12 +0530

    Don't build as library

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
-const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-/*     build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/main.ts"),
-            name: "chatterbox",
-            fileName: "chatterbox.js",
-        },
-    }, */
     server: {
         fs: {
             // Allow serving files from hydrogen-web/target (for fonts and images)
