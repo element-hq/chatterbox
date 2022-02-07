6fa7be4096371af6baa8c5dc814aee1f32b3d1c7
Midhun Suresh <midhunr@element.io>
2022-02-07 13:24:04 +0530

    Add explaining comments to config type

--- src/types/IChatterboxConfig.ts
+++ src/types/IChatterboxConfig.ts
@@ -1,5 +1,7 @@
 export interface IChatterboxConfig {
 	homeserver: string;
+    // internal room-id of the room to which chatterbox should join
 	auto_join_room: string;
+    // string that is to be prepended to the generated random usernames
     username_prefix: string;
 }
