a8c67f1226e447f23620d625e2d02d89540e161a
RMidhunSuresh <hi@midhun.dev>
2022-02-10 19:20:35 +0530

    Also delete manifest from build

--- scripts/after-build.sh
+++ scripts/after-build.sh
@@ -3,5 +3,6 @@ echo "Running after-build tasks ..."
 find target/assets/ -name "parent.*.js" -exec rm -vf {} \;
 find target/assets/ -name "parent.*.css" -exec rm -vf {} \;
 rm -vf target/index.html
+rm -vf target/manifest.json
 mv -v target/parent/assets/* target/assets/
 rm -rfv target/parent
