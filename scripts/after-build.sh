0fe83bd66513dd24177ee3ff0eaca1697d886553
Midhun Suresh <midhunr@element.io>
2022-02-10 07:29:58 +0000

    Use git-lab ci to do automatic build and release on tag creation

---
+++ scripts/after-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Running after-build tasks ..."
+find target/assets/ -name "parent.*.js" -exec rm -vf {} \;
+find target/assets/ -name "parent.*.css" -exec rm -vf {} \;
+rm -vf target/index.html
+mv -v target/parent/assets/* target/assets/
+rm -rfv target/parent
