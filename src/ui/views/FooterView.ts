29bacfdde0fe98acf3c183248df6787b86fef7c8
RMidhunSuresh <hi@midhun.dev>
2022-07-05 12:53:01 +0530

    Footer view should use footer vm

--- src/ui/views/FooterView.ts
+++ src/ui/views/FooterView.ts
@@ -1,15 +1,31 @@
 import { TemplateView } from "hydrogen-view-sdk";
 import chatterboxLogo from "../res/chat-bubbles.svg";
+import { FooterViewModel } from "../../viewmodels/FooterViewModel";
 
-export class FooterView extends TemplateView {
-    render(t) {
-        return t.div({ className: "FooterView" },
-            [
-                t.div([
-                    t.img({ src: chatterboxLogo, className: "FooterView_logo" }),
-                    t.span({ className: "FooterView_chatterbox-branding" }, "Chatterbox"),
-                ]),
-                t.div({ className: "FooterView_matrix-branding" }, ["Powered by Matrix"])
-            ]);
+export class FooterView extends TemplateView<FooterViewModel> {
+    constructor(value) {
+        super(value);
+    }
+
+    render(t, vm) {
+        return t.div({ className: "FooterView" }, [
+            t.div([
+                t.img({ src: chatterboxLogo, className: "FooterView_logo" }),
+                t.button(
+                    {
+                        className: "FooterView_chatterbox-branding",
+                        onClick: () => vm.openChatterboxLink(),
+                    },
+                    "Chatterbox"
+                ),
+            ]),
+            t.button(
+                {
+                    className: "FooterView_matrix-branding",
+                    onClick: () => vm.openMatrixLink(),
+                },
+                ["Powered by Matrix"]
+            ),
+        ]);
     }
 }
